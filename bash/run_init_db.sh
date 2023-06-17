19ea2b96a2dce848a9db54665102e7c3b847dc13
vakulakarakula <vakula-07@mail.ru>
2023-06-18 00:10:36 +0300

    Create bash script

--- bash/run_init_db.sh
+++ bash/run_init_db.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+docker pull postgres:latest
+docker run --name postgres-db-vakula -e POSTGRES_PASSWORD="@sde_password012" -e POSTGRES_USER="test_sde" -e POSTGRES_DB="demo" -v $HOME/sde_test_db:/mnt -d postgres
+echo "shushut waiting, bro!"
+sleep 15s
+docker exec postgres-db-vakula psql -U test_sde -d demo -f /mnt/sql/init_db/demo.sql
