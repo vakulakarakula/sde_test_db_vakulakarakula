5d544fa304c7d876d1b8226bd4f1faeb11be6795
vakulakarakula <vakula-07@mail.ru>
2023-06-19 17:53:55 +0300

    Script correction home - pwd

--- bash/run_init_db.sh
+++ bash/run_init_db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker pull postgres:latest
-docker run --name postgres-db-vakula -p 5432:5432 -e POSTGRES_PASSWORD="@sde_password012" -e POSTGRES_USER="test_sde" -e POSTGRES_DB="demo" -v $HOME/sde_test_db:/mnt -d postgres
+docker run --name postgres-db-vakula -p 5432:5432 -e POSTGRES_PASSWORD="@sde_password012" -e POSTGRES_USER="test_sde" -e POSTGRES_DB="demo" -v $(pwd)/sde_test_db:/mnt -d postgres
 echo "shushut waiting, bro!"
 sleep 15s
 docker exec postgres-db-vakula psql -U test_sde -d demo -f /mnt/sql/init_db/demo.sql
